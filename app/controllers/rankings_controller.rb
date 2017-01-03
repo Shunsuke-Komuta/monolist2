c8983457573c17b5963e2fb48678c1bdcba6aa80
Shunsuke-Komuta <komuta@level-five.jp>
2017-01-03 18:56:58 +0900

    課題10. 2. 正しい順位が表示されていること 3. ランキングの順位がわかるようデザインを整えること 5. ランキングを確認するために、5個以上のデータが作成されている

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,7 +1,13 @@
 class RankingsController < ApplicationController
   def have
+    have_ids = Have.group(:item_id).order('count_item_id desc').limit(10).count('item_id').keys
+    @items = Item.find(have_ids).sort_by{|o| have_ids.index(o.id)}
+    render 'show'
   end
 
   def want
+    want_ids = Want.group(:item_id).order('count_item_id desc').limit(10).count('item_id').keys
+    @items = Item.find(want_ids).sort_by{|o| want_ids.index(o.id)}
+    render 'show'
   end
 end
